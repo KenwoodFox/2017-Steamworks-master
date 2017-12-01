05e3a871ab359db9fa8fda81604e5889631e14f9
KenwoodFox <31870999+KenwoodFox@users.noreply.github.com>
2017-12-01 16:26:09 -0500

    bjork

--- frc-1721.github.io-master/newsite/js/index.js
+++
@@ -1,5 +0,0 @@
-if (window.innerWidth > 600) {
-    document.getElementsByTagName('video')[0].src = 'assets/video.mp4';
-    var video = document.getElementsByTagName('video')[0];
-    video.muted = true;
-}
